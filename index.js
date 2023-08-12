30d8461ea68846e745c7d086f43eda16dded196a
vipulchandan <plenteousvipul90009@gmail.com>
2023-08-12 13:47:45 +0530

    updates

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const FormData = require('form-data');
 const fs = require('fs');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 dotenv.config();
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.static('public'));
 
 const storage = multer.diskStorage({
